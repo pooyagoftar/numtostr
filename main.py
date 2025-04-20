165011139e27cce3074c4c9cf76b30b1ea6c221a
pooyagoftar <pgoftar@gmail.com>
2025-04-20 19:47:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def main(num):
     return str(num)
 
-num = 123
+def takenum ():
+    return int(input("Enter your num:"))
+    
 
 
 
 if __name__ == "__main__":
+    num = takenum()
     print(f"{type(main(num))} {main(num)}")
 
